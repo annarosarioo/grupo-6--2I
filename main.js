6c0edf444a3d4d7fea2b41d28f0d6804894c3ddb
annarosarioo <131979194+annarosarioo@users.noreply.github.com>
2023-09-12 08:12:40 -0300

    e

--- main.js
+++ main.js
@@ -2,4 +2,11 @@
 document.querySelector('.#som_tecla_pom').play();
 }
 
+const ListaDeSons = document.querySelectorA11('.tecla');
 document.querySelector('.#tecla_pom').onclick = TocaSomPom;
+let Contador = 0;
+
+while () {
+
+ListaDeSons[0].onclick = TocaSomPom;
+}
