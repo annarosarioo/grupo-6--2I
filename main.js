08ef0b498242b51de193f6e9a1837261e1e6efa4
annarosarioo <131979194+annarosarioo@users.noreply.github.com>
2023-09-12 08:19:18 -0300

    A

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
-    function TocaSomPom () {
-document.querySelector('.#som_tecla_pom').play();
+function TocaSomPom () {
+    document.querySelector('.#som_tecla_pom').play();
 }
 
 const ListaDeSons = document.querySelectorA11('.tecla');
-document.querySelector('.#tecla_pom').onclick = TocaSomPom;
+
 let Contador = 0;
 
-while () {
-
-ListaDeSons[0].onclick = TocaSomPom;
+while (Contador < 9) {
+   ListaDeSons[0].onclick = TocaSomPom;
+   Contador = Contador + 1;
+   console.log(Contador);
 }
+
+
