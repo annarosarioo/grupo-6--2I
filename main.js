5d17efab82af3ac23cb7153c5faf0aaf1f5d6b7a
annarosarioo <131979194+annarosarioo@users.noreply.github.com>
2023-09-19 07:57:01 -0300

    ;

--- main.js
+++ main.js
@@ -2,14 +2,22 @@ function TocaSomPom () {
     document.querySelector('#som_tecla_pom').play();
 }
 
+
+
+//constante = Lista dos botões
 const ListaDeSons = document.querySelectorAll('.tecla');
 
+//variavel = 0 fixo
 let Contador = 0;
 
+//enquanto - conição
 while (Contador < 9) {
+   //acessando um elemento da lista 
    ListaDeSons[Contador].onclick = TocaSomPom;
-   Contador = Contador + 1;
+   //mudei o valor da variavel
+    Contador = Contador + 1;
+   //console ta onversando comigo
    console.log(Contador);
-}
+} 
 
 
